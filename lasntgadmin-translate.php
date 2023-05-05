390c23182a1617676a0d58c2783be48d3159d003
Eric <emuigai5@gmail.com>
2023-05-05 13:15:44 +0300

    bump version

--- lasntgadmin-translate.php
+++ lasntgadmin-translate.php
@@ -3,7 +3,7 @@
  * Plugin Name:       LASNTG Admin Translate
  * Plugin URI:        https://github.com/fioru-software/lasntgadmin-plugin_template
  * Description:       Translations.
- * Version:           1.0.0
+ * Version:           1.1.0
  * Requires PHP:      7.2
  * Text Domain:       lasntgadmin
  * Domain Path:       /languages
