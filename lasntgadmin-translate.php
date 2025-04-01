21a7d44965535d30d1f32a00fcf44ec2b05fb261
Charles Mulder <7zh6dj9f9@mozmail.com>
2025-04-01 17:02:43 +0100

    activate translations (#12)
    * activate translations
    
    * stable release

--- lasntgadmin-translate.php
+++ lasntgadmin-translate.php
@@ -3,7 +3,7 @@
  * Plugin Name:       LASNTG Admin Translate
  * Plugin URI:        https://github.com/fioru-software/lasntgadmin-plugin_template
  * Description:       Translations.
- * Version:           1.2.13
+ * Version:           1.2.14
  * Requires PHP:      7.2
  * Text Domain:       lasntgadmin
  * Domain Path:       /languages
